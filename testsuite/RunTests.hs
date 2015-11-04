99b742c08a6991dfa6f488c713d30c2ac1bd9ba8
Savor d'Isavano <newelevenken@163.com>
2015-11-04 11:17:03 +0800

    Fixed up the test ground

--- testsuite/RunTests.hs
+++ testsuite/RunTests.hs
@@ -1,4 +1,22 @@
 module RunTests where
 
-gg :: Bool
-gg = False
+import Distribution.TestSuite.QuickCheck
+import Tests.Game
+
+tests :: IO [Test]
+tests = return [
+  testProperty "Succeeds" True,
+  testProperty "Fails" False,
+  mayFail
+  ]
+
+
+mayFail :: Test
+mayFail = testGroup "May fail" [
+  testProperty "Maybe fails" neqNegation,
+  testProperty "Probably fails" (not . neqNegation)
+  ]
+
+neqNegation :: Int -> Bool
+neqNegation x = x /= -x
+
---
+++ testsuite/Tests/Game.hs
@@ -0,0 +1,7 @@
+module Tests.Game where
+
+import Distribution.TestSuite.QuickCheck
+import qualified Game as G
+
+tests :: IO [Test]
+tests = return []
